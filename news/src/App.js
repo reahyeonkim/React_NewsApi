9bedb1be1a664dccbb5ebd3d7edd9be55e5f8962
raehyunkim <sscv92@gmail.com>
2022-05-19 18:52:36 +0900

    news데이터불러오기성공

--- news/src/App.js
+++ news/src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
+  //뉴스 데이터 useState 로 상태관리 하기
+  const [news, setNews] = useState(null);
+  const newsData = async () => {
+    try {
+      const response = await axios.get(
+        "https://newsapi.org/v2/top-headlines?country=kr&apiKey=ea2b4df1017c4c8c8fe34368d1d391c6"
+      );
+      setNews(response.data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+        <div>
+          <div>
+            <button onClick={newsData}>뉴스데이터</button>
+          </div>
+          <div>
+            {news && <textarea rows={7} value={JSON.stringify(news, null , 2)}/>}
+          </div>
+        </div>
+    </>
   );
 }
 
